8ed059a4c8d7a0bd2816f773d5d7792db414a5b4
Fabien MILLERAND <fab@millerf.com>
2019-07-31 16:42:57 +0200

    Clean up app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import {Component} from '@angular/core';
 import {
-  AttendanceType,
-  AttendanceTypeUnanswered,
   CastellerInterface,
   CastellerLloc,
   PinyaCastell,
-  SectionPinya
 } from '../../projects/ng-pinyes-d3/src/lib/pinya.model';
 import {PinyaFactory} from '../../projects/ng-pinyes-d3/src/lib/ng-pinyes-factory.class';
 
 class Casteller implements CastellerInterface {
   pk = Math.floor(Math.random() * 10000) + 1;
-  name = 'ttest';
+  name = 'test name';
 
   getName() {
     return this.name;
@@ -26,32 +23,15 @@ class Casteller implements CastellerInterface {
 })
 export class AppComponent {
   pinya: PinyaCastell;
-  editMode = false;
+  editMode = true;
 
   constructor() {
-    //
-    // this.pinya = PinyaFactory.generate(4, 4, 4, 4);
-    // this.pinya.sections[0].mans[0].casteller= new Casteller();
-    // this.pinya.sections[0].laterals.esquerra[2].casteller= new Casteller();
-    // this.pinya.sections[0].laterals.dreta[3].casteller= new Casteller();
-    // this.pinya.sections[1].mans[0].casteller= new Casteller();
-    // this.pinya.sections[1].laterals.esquerra[0].casteller= new Casteller();
-    // this.pinya.sections[1].laterals.dreta[0].casteller= new Casteller();
-    // this.pinya.sections[1].vents[0].casteller= new Casteller();
 
     this.pinya = PinyaFactory.generate(1, 4, 4, 4);
     this.pinya.sections[0].mans[0].casteller= new Casteller();
     this.pinya.sections[0].laterals.esquerra[2].casteller= new Casteller();
     this.pinya.sections[0].laterals.dreta[3].casteller= new Casteller();
-    //
-    // setTimeout(() => {
-    //   this.pinya.sections[1].vents = this.pinya.sections[1].vents.slice(2, 4);
-    //   this.pinya = new PinyaCastell().assign(this.pinya);
-    // }, 4000)
 
-    // setTimeout(() => {
-      this.editMode = ! this.editMode;
-    // }, 4000)
   }
 
   clickOnCastellerLloc(lloc: CastellerLloc) {
