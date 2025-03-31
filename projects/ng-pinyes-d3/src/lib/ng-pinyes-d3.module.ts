58a9b34b06ce42c409daab62126b55336a0fde9f
Fabien MILLERAND <fab@millerf.com>
2025-03-31 12:55:00 +0200

    Code for 0.1.8

--- projects/ng-pinyes-d3/src/lib/ng-pinyes-d3.module.ts
+++ projects/ng-pinyes-d3/src/lib/ng-pinyes-d3.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NgPinyesD3Component } from './ng-pinyes-d3.component';
+import { ComponentNameComponent } from './component-name/component-name.component';
 
 
 
 @NgModule({
-  declarations: [NgPinyesD3Component],
+  declarations: [NgPinyesD3Component, ComponentNameComponent],
   imports: [
   ],
   exports: [NgPinyesD3Component]
